961cd808ab35ee1b49b2e086c15f44a0f7ed1207
vand <vanderleichagas42@gmail.com>
2025-10-15 19:04:49 -0300

    feat: add form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,27 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function handleSave(){
+  console.log('save')
+}
 
+function App() {
+  const [amount, setAmount] = useState(0)
+  const [description, setDescription] = useState('')
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+            <label htmlFor="description">Transaction description</label>
+            <input name='description' value={description} type="text" />
+            <label htmlFor="amount">Transaction amount</label>
+            <input name='amount' value={amount} type="number" />
+            <button onClick={handleSave}>Submit</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
